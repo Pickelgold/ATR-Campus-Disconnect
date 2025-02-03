17ed6e745c6d5ec108895294f88753123de308a8
Pickelgold <edwin@edwinhe.com>
2025-02-03 10:44:34 -0800

    Change navigation icons to mirror the original app

--- app/src/main/java/com/example/atrcampusdisconnect/MainActivity.kt
+++ app/src/main/java/com/example/atrcampusdisconnect/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_help, R.id.navigation_biometrics, R.id.navigation_logout
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
